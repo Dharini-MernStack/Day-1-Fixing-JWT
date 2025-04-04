ae0630e198e3c9a64e6f02dd11572b23a682f9df
srikeerthireddy <srikeerthireddy24@gmail.com>
2025-04-04 17:28:51 +0530

    JWT Implementation

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,34 +1,59 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcrypt"); // Not used correctly
+const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
 const router = express.Router();
 router.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  
- 
-
-  const user = new User({ username, password }); 
-  await user.save();
-  res.json({ message: "User registered successfully" });
+  if(!username || !password){
+    return res.status(400).json({ message: "Please fill all fields"});
+  }
+  try{
+    const existingUser = await User.findOne({username});
+    console.log(existingUser)
+    if(existingUser){
+      return res.status(400).json({message: "User already exists"});
+    }
+    // Hash password before saving to database
+    const hashedPassword = await bcrypt.hash(password,10);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+    res.json({ message: "User registered successfully" });
+  }catch(error){
+    res.status(500).json({ message: "Error registering user"});
+  }
 });
 
-
+const jwtSecret = process.env.JWT_SECRET;
+if(!jwtSecret){
+  console.error("JWT seceret is not defined in .env file");
+}
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ username });
-
-  if (!user) {
-    return res.status(400).json({ message: "User not found" }); 
+  if(!username || !password){
+     return res.status(400),json({message: "Please fill all fields"});
   }
+  try{
+    const user = await User.findOne({ username });
 
-  if (user.password !== password) { 
-    return res.status(400).json({ message: "Invalid credentials" });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" }); 
+    }
+  
+    if (user.password !== password) { 
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const isPasswordValid = await bcrypt.compare(password,user.password);
+    if(!isPasswordValid){
+      return res.status(400).json({ message: "Invalid credientials"});
+    }
+  
+    const token = jwt.sign({ userId: user._id }, jwtSecret, { expiresIn: "1h" }); 
+    res.json({ token });
+  }catch(error){
+    res.status(500).json({message:"Error logging in"});
   }
-
-  const token = jwt.sign({ userId: user._id }, "insecuresecret", { expiresIn: "1h" }); 
-  res.json({ token });
 });
 
 module.exports = router;
